b703b3d9213fbc48195a17cde59d9767d6e41821
nunoluciano <nunoluciano@yahoo.com>
2022-03-04 21:33:55 +0100

    ⚡ 🔨 🎨 Code refactoring for XCL PHP7

--- html/modules/pico/notification_update.php
+++ html/modules/pico/notification_update.php
@@ -1,9 +1,13 @@
 <?php
-require '../../mainfile.php' ;
-if( ! defined( 'XOOPS_TRUST_PATH' ) ) die( 'set XOOPS_TRUST_PATH in mainfile.php' ) ;
 
-$mydirname = basename( dirname( __FILE__ ) ) ;
-$mydirpath = dirname( __FILE__ ) ;
+require '../../mainfile.php' ;
+
+if( ! defined( 'XOOPS_TRUST_PATH' ) ) {
+	die( 'set XOOPS_TRUST_PATH in mainfile.php' );
+}
+
+$mydirname = basename(__DIR__) ;
+$mydirpath = __DIR__;
 $mydirurl = XOOPS_URL.'/modules/'.$mydirname;
 
 require $mydirpath.'/mytrustdirname.php' ; // set $mytrustdirname
@@ -11,4 +15,3 @@ require $mydirpath.'/mytrustdirname.php' ; // set $mytrustdirname
 $_GET['page'] = basename( __FILE__ , '.php');
 
 require XOOPS_TRUST_PATH.'/modules/'.$mytrustdirname.'/main.php' ;
-?>
